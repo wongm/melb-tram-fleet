7b6b49a05a36003eb37c648940f64e13a951cf3e
Marcus Wong <marcusthomaswong@gmail.com>
2018-01-19 08:20:34 +1100

    Add tram route allocation WIP

---
+++ routes.php
@@ -0,0 +1,21 @@
+<?php
+
+$tram_routes['Z3'] = array(57, 82);
+
+$tram_routes['A1'] = array();
+
+$tram_routes['A2'] = array();
+
+$tram_routes['C'] = array(109, 48);
+
+$tram_routes['C2'] = array(96);
+
+$tram_routes['D1'] = array();
+
+$tram_routes['D2'] = array();
+
+$tram_routes['E'] = array(11, 86, 96);
+
+$tram_routes['E2'] = array(11, 86, 96);
+
+?>
