39452dd946ac6d716a3d5d8f8067e70052ea8a66
marcusw <marcusw@pageuppeople.com>
2018-01-19 10:11:14 +1100

    Update tram fleet / route mapping list

--- routes.php
+++ routes.php
@@ -1,21 +1,25 @@
 <?php
 
-$tram_routes['Z3'] = array(57, 82);
+$tram_routes['W'] = array(35);
 
-$tram_routes['A1'] = array();
+$tram_routes['Z3'] = array(1,3,"3a",5,6,16,72,57,58,64,67,82); //19?
 
-$tram_routes['A2'] = array();
+$tram_routes['A1'] = array("3a",3,12,30,64,67);
 
-$tram_routes['C'] = array(109, 48);
+$tram_routes['A2'] = array(12,30,48,78); //75,109?
+
+$tram_routes['B2'] = array(1,3,"3a",3,6,11,19,58,59,64,67,70,75,86);
+
+$tram_routes['C'] = array(48,109);
 
 $tram_routes['C2'] = array(96);
 
-$tram_routes['D1'] = array();
+$tram_routes['D1'] = array(5,6,16,58,72);
 
-$tram_routes['D2'] = array();
+$tram_routes['D2'] = array(6,19);
 
-$tram_routes['E'] = array(11, 86, 96);
+$tram_routes['E'] = array(11,86,96);
 
-$tram_routes['E2'] = array(11, 86, 96);
+$tram_routes['E2'] = array(11,86,96);
 
 ?>
