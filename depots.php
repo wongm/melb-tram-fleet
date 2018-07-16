534465ba127f897d00e078e5848f4f32564f10ea
marcusw <marcusw@pageuppeople.com>
2018-07-16 10:09:35 +1000

    Add depot listing

---
+++ depots.php
@@ -0,0 +1,19 @@
+<?php
+
+$tram_depots['Brunswick'] = array(1, 6, 19);
+
+$tram_depots['Camberwell'] = array(70, 75);
+
+$tram_depots['Essendon'] = array(57, 58, 59, 82);
+
+$tram_depots['Glenhuntly'] = array(3, "3a", 64, 67);
+
+$tram_depots['Kew'] = array(48, 78, 109);
+
+$tram_depots['Malvern'] = array(5, 6, 16, 58, 72);
+
+$tram_depots['Preston'] = array(11, 86);
+
+$tram_depots['Southbank'] = array(12, 30, 35, 96);
+
+?>
