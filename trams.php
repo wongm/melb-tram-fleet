b3edfc52c2f910d19c4283bde26dfae19c78ac52
Marcus Wong <marcusthomaswong@gmail.com>
2020-11-04 09:12:45 +1100

    Update for W8 class trams

--- trams.php
+++ trams.php
@@ -3,12 +3,12 @@
 //SW6
 $melbourne_trams['SW6']['low-floor'] = false;
 $melbourne_trams['SW6']['air-conditioned'] = false;
-$melbourne_trams['SW6']['trams'] = array(888, 925, 928, 937, 938, 939, 961);
+$melbourne_trams['SW6']['trams'] = array(925, 937, 938, 939);
 
 //W6
 $melbourne_trams['W6']['low-floor'] = false;
 $melbourne_trams['W6']['air-conditioned'] = false;
-$melbourne_trams['W6']['trams'] = array(983, 1000);
+$melbourne_trams['W6']['trams'] = array(1000);
 
 //W7
 $melbourne_trams['W7']['low-floor'] = false;
@@ -18,7 +18,7 @@ $melbourne_trams['W7']['trams'] = array(1020);
 //W8
 $melbourne_trams['W8']['low-floor'] = false;
 $melbourne_trams['W8']['air-conditioned'] = false;
-$melbourne_trams['W8']['trams'] = array(946, 957, 959, 981, 1010);
+$melbourne_trams['W8']['trams'] = array(856, 888, 928, 946, 957, 959, 961, 981, 983, 1010);
 
 //Z1
 $melbourne_trams['Z1']['low-floor'] = false;
